71aad2972c181c21ada772964101051e540f2475
hamzasaniCoder <hamzasani.coder@gmail.com>
2025-02-28 16:33:42 +0500

    fix some user controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,10 @@
-import {asyncHandler} from "../utils/asyncHandler.js"
-import {ApiError} from "../utils/ApiError.js"
-import {User} from "../models/user.model.js"
-import {uploadOnCloudinary} from "../utils/cloudinary.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { ApiError } from "../utils/ApiError.js"
+import { User } from "../models/user.model.js"
+import { uploadOnCloudinary } from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
 
-const registerUser = asyncHandler( async (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
     // get user details from frontend
     // validation - not empty
     // check if user already exists: username, email
@@ -15,7 +15,8 @@ const registerUser = asyncHandler( async (req, res) => {
     // check for user creation
     // return res
 
-    const {fullName, password, email, username} =  req.body;
+    const { fullName, password, email, username } = req.body;
+    console.log("Files Data:", req.files);
 
     if (
         [fullName, email, username, password].some((field) => field?.trim() === "")
@@ -23,16 +24,24 @@ const registerUser = asyncHandler( async (req, res) => {
         throw new ApiError(400, "All fields are required")
     }
 
-    const existedUser = User.findOne({
-        $or : [{username}, {email}]
+    const existedUser = await User.findOne({
+        $or: [{ username }, { email }]
     })
 
     if (existedUser) {
         throw new ApiError(409, "User with username and email already exists")
     }
-
+    console.log(req.files);
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // console.log("Avatar Path:", avatarLocalPath);
+
+    let coverImageLocalPath;
+    if (
+        req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0
+    ) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar is required")
@@ -43,18 +52,18 @@ const registerUser = asyncHandler( async (req, res) => {
 
     if (!avatar) {
         throw new ApiError(400, "Avatar is required.")
-        
+
     }
 
     const user = await User.create({
-        fullName, 
-        password, 
+        fullName,
+        password,
         email,
-        avatar : avatar.url,
-        coverImage : coverImage?.url || "",
-        username : username.toLowerCase()
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        username: username.toLowerCase()
     })
-    
+
     const createdUser = await User.findById(user._id).select(
         "-password -refreshToken"
     )
@@ -64,7 +73,7 @@ const registerUser = asyncHandler( async (req, res) => {
     }
 
     return res.status(201).json(
-        ApiResponse(200, createdUser, "User Registered Successfully")
+       new ApiResponse(200, createdUser, "User Registered Successfully")
     )
 })
 
